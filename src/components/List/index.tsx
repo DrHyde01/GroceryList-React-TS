192c4234da332a770e3550f3b2ba8b0611b4e5a6
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-05-06 14:43:07 +0200

    🐛 Try to fixing handleChange bug

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -26,10 +26,8 @@ const List: React.FC = () => {
   const [suggestionShow, setSuggestionShow] = useState<boolean>(false);
 
   //Add item from the input
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement> | React.MouseEvent<HTMLInputElement>
-  ) => {
-    const query = e.currentTarget.value;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
     setItem(query);
     if (query.length > 1) {
       const suggestionFilter = data.filter(
